934e45cde466eea10f547e719832f9b4f57b44e6
Kenny Barsch <masterlefty@gmail.com>
2016-02-13 22:11:57 +0000

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- workspace.js
+++ workspace.js
@@ -229,6 +229,23 @@ cpdefine("inline:com-chilipeppr-workspace-masterlefty", ["chilipeppr_ready"], fu
         },
         
         // insert callbacks here
+        load3dviewer: function(callback) {
+            chilipeppr.load(
+                "#com-chilipeppr-3dviewer",
+                "http://raw.githubusercontent.com/chilipeppr/widget-3dviewer/master/auto-generated-widget.html",
+                function() {
+                    // Callback after widget loaded into #myDivWidgetInsertedInto
+                    cprequire(
+                        ["inline:com-chilipeppr-widget-3dviewer"], // the id you gave your widget
+                        function(mywidget) {
+                            // Callback that is passed reference to your newly loaded widget
+                            console.log("My widget just got loaded.", mywidget);
+                            mywidget.init();
+                        }
+                    );
+                }
+            );
+        }, // end 3D Viewer
         
     };
 });
